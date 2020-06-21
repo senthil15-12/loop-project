c306506c8dc8a13fbb62719b95709c7b5bb722de
senthil15-12 <67093765+senthil15-12@users.noreply.github.com>
2020-06-21 13:10:27 +0530

    Update positive numbers (1).py
    programs to print positive numbers and fibonacci series

--- positive numbers (1).py
+++ positive numbers (1).py
@@ -2,19 +2,24 @@
 # coding: utf-8
 
 # In[21]:
-
+program 1 a
 
 list1=[12,-7,5,64,-14]
 for i in list1:
     if i>0:
         print(i)
     
-    
+    output:
+        
+
+12
+5
+64
 
 
 # In[22]:
 
-
+program 1 b
 list1=[12,14,-95,3]
 for i in list1:
     if i>0:
@@ -24,9 +29,39 @@ for i in list1:
 # In[ ]:
 
 
+output of 1 b:
+12
+14
+3
 
 
+program 2 
 
+a=0
+b=1
+c=0
+print(a)
+print(b)
+while (c<=50):
+    c=a+b
+    a=b
+    b=c
+    print(c)
+    
+    output:
+
+
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
 # In[ ]:
 
 
