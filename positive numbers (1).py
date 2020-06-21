93f4bd52b22e02b802f828b7e3fb9057315d92b0
senthil15-12 <67093765+senthil15-12@users.noreply.github.com>
2020-06-21 13:06:55 +0530

    Add files via upload
    positive numbers

---
+++ positive numbers (1).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i>0:
+        print(i)
+    
+    
+
+
+# In[22]:
+
+
+list1=[12,14,-95,3]
+for i in list1:
+    if i>0:
+        print(i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
